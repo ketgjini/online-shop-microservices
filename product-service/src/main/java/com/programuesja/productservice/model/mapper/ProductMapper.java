6ad9aad36bf28c42f0e06734c5b7f4a939d65cbf
Programuesja <gjiniketrina@gmail.com>
2023-11-06 01:01:48 +0100

    added api-gateway

--- product-service/src/main/java/com/programuesja/productservice/model/mapper/ProductMapper.java
+++ product-service/src/main/java/com/programuesja/productservice/model/mapper/ProductMapper.java
@@ -8,11 +8,22 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public abstract class ProductMapper {
     public abstract ProductResponse toResponse(final Product product);
     public abstract Product toProduct(final ProductRequest productRequest);
-    public abstract List<ProductResponse> toResponseList(final List<Product> products);
+
+    public List<ProductResponse> toResponseList(List<Product> products) {
+        if (products == null) {
+            return Collections.emptyList();
+        }
+
+        return products.stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
+    }
 }
